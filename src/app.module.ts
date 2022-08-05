535d29c39b5c93b7555afc1266e49de240c9391a
fengqi <feengqi@gmail.com>
2022-08-05 11:19:17 +0800

    添加swagger和Restfull users

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [UsersModule, PostsModule],
+  imports: [UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
